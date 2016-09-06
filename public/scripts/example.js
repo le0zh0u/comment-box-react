5dd4142dba301167b71ac99a36b1776cf473e158
le0zh0u <492753662@qq.com>
2016-09-06 23:59:02 +0800

    add comments form

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -68,11 +68,26 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+	handleSubmit: function(e) {
+		e.preventDefault();
+		var author = ReactDOM.findDOMNode(this.refs.author).value.trim();
+		var text = ReactDOM.findDOMNode(this.refs.text).value.trim();
+		if(!text || !author) {
+			return;
+		}
+
+		//TODO: send request to the server
+		ReactDOM.findDOMNode(this.refs.author).value = '';
+		ReactDOM.findDOMNode(this.refs.text).value = '';
+		return;
+	},
 	render: function() {
 		return (
-			<div className="commentForm">
-				Hello, world! I am a CommentForm.
-			</div>
+			<form className="commentForm" onSubmit={this.handleSubmit}>
+				<input type="text" placeholder="Your name" ref="author" />
+				<input type="text" placeholder="Say something..." ref="text"/>
+				<input type="submit" value="Post" />	
+			</form>
 		)
 	}
 });
